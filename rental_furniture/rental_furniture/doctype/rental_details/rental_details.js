9518aeaeae936a27daf81baf8e0ce439c238b5d6
Jishnu <jishnuvv2016@gmail.com>
2025-11-13 05:01:05 +0000

    rental changes

---
+++ rental_furniture/rental_furniture/doctype/rental_details/rental_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, jishnu and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Rental Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ rental_furniture/rental_furniture/doctype/rental_details/test_rental_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, jishnu and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRentalDetails(FrappeTestCase):
+	pass
