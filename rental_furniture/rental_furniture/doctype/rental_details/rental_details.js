a7a8afc486cc61207cb581ac543ba6657b1a7190
Jishnu <jishnuvv2016@gmail.com>
2025-11-17 15:27:35 +0000

    feat:updated Workspace

--- rental_furniture/rental_furniture/doctype/rental_details/rental_details.js
+++ rental_furniture/rental_furniture/doctype/rental_details/rental_details.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2025, jishnu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Rental Details", {
-// 	refresh(frm) {
+frappe.ui.form.on("Rental Details", {
+refresh(frm) {
+        if (frm.doc.docstatus == 1) {
+            frm.add_custom_button(__('Sales Invoice'), function() {
+                frappe.call({
+                    method: "rental_furniture.rental_furniture.doctype.rental_details.rental_details.make_sales_invoice",
+                    args: {
+                        name: frm.doc.name
+                    },
+                    callback: function(r) {
+                        if (r.message) {
+                            frappe.show_alert({
+                                message: "sales Invoice is Created",
+                                indicator: 'green'
+                            }, 5);
+                            frappe.set_route('Form', 'Sales Invoice', r.message);
+                        }
+                    }
+                });
+            }, __('Create'));
+        }
 
-// 	},
-// });
+	},
+});
--- rental_furniture/rental_furniture/doctype/rental_details/rental_details.py
+++ rental_furniture/rental_furniture/doctype/rental_details/rental_details.py
@@ -1,9 +1,32 @@
 # Copyright (c) 2025, jishnu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RentalDetails(Document):
 	pass
+
+
+@frappe.whitelist()
+def make_sales_invoice(name):
+    rental = frappe.get_doc("Rental Details", name)
+
+    si = frappe.get_doc({
+        "doctype": "Sales Invoice",
+        "customer": rental.customer,
+    })
+    for item in rental.rental_items:
+        si_item = si.append("items", {})
+        si_item.item_code = item.item_code
+        si_item.item_name = item.item
+        si_item.qty = item.qty
+        si_item.rate = item.rental_rate
+       
+    si.insert(ignore_permissions=True)
+    si.submit()
+
+    frappe.msgprint(f"Sales Invoice {si.name} created successfully")
+
+    return si.name
